f983941c27164d37e6ab0423e6a87810f7773af8
soham1209 <sohamns1234@gmail.com>
2025-11-08 14:14:52 +0530

    feat: Add chess.js dependency and integrate Board component into Game

--- FrontEnd/src/componets/Board.tsx
+++ FrontEnd/src/componets/Board.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import type { Square, PieceSymbol, Color } from "chess.js";
 
-function Board() {
-  return (
-    <div>Board</div>
-  )
+function Board({
+  board,
+}: {
+  board: ({
+    square: Square;
+    type: PieceSymbol;
+    color: Color;
+  } | null)[][];
+}) {
+  return <div>
+    {board.map((row, rowIndex) => (
+      <div key={rowIndex} style={{ display: 'flex' }}>
+        {row.map((cell, colIndex) => (
+          <div key={colIndex} style={{ width: '40px', height: '40px', border: '1px solid black' }}>
+            {cell ? cell.type : null}
+          </div>
+        ))}
+      </div>
+    ))}
+  </div>;
 }
 
-export default Board
+export default Board;
--- FrontEnd/src/pages/Game.tsx
+++ FrontEnd/src/pages/Game.tsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Button from "../componets/Button";
-import useSocket from "../hook/UseScoket";
+import useSocket from "../hook/useScoket";
+import Board from "../componets/Board";
+import {Chess} from 'chess.js';
+
 
 //TODO: Move to a separate file,their is code repitaion
 export const INIT_GAME = "init_game";
@@ -9,20 +12,26 @@ export const GAME_OVER = "game_over";
 
 function Game() {
   const socket = useSocket();
-  if (!socket) return <div>Connecting to server...</div>;
+  const [chess,setChess] = useState(new Chess());
+  const [board,setBoard] = useState(chess.board());
+
 
   useEffect(() => {
     if (!socket) return;
     socket.onmessage = (event) => {
-        
       const message = JSON.parse(event.data);
       console.log("Received message:", message);
 
       switch (message.type) {
         case INIT_GAME:
+          setChess(new Chess());
+          setBoard(chess.board());
           console.log("Game initialized:", message.payload);
           break;
         case MOVE:
+          const move = message.payload;
+          chess.move(move);
+          setBoard(chess.board());// Update board state  
           console.log("Move received:", message.payload);
           break;
         case GAME_OVER:
@@ -33,16 +42,23 @@ function Game() {
       }
     };
   }, [socket]);
+  
+  if (!socket) return <div>Connecting to server...</div>;
+
 
   return (
     <div>
-      Game
-      <Button
-        onClick={() => {
-          socket.send(JSON.stringify({ type: INIT_GAME }));
-        }}
-        children={"Start Game"}
-      />
+      <div>
+        <Board board={board} />
+      </div>
+      <div>
+        <Button
+          onClick={() => {
+            socket.send(JSON.stringify({ type: INIT_GAME }));
+          }}
+          children={"Start Game"}
+        />
+      </div>
     </div>
   );
 }
